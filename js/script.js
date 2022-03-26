d15e7b8eccd0623ffbf6c4519a86eb6ec7a34afa
Ticapril <gb.oliveirati@gmail.com>
2022-03-26 19:53:37 -0300

    alterações na lógica do jogo, no css3 e na estrutura do html5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-   //biblioteca de funções
-   function getRandomInt(min, max){
+//variaveis
+let arrayWords = ["pikachu","charmander","dragonite","psyduck"];
+let randomArrayElement;
+let tentativas = 0;
+
+
+
+//biblioteca de funções
+function getRandomInt(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
    return Math.floor(Math.random() * (max -min)) + min;
@@ -10,6 +17,8 @@
 function createWord() {
     //pego o elemento div da página onde fica os inputs
     let referencia = document.getElementById("boxWord");
+    let vetorInputs = document.querySelectorAll("input.boxSize:not(input#letraChute.boxSize)");
+    
 
    if(referencia.firstChild === null){
       let arrayChoose = randomWord("Pokemon");
@@ -20,9 +29,19 @@ function createWord() {
         }
    }
    else {
-       alert("Palavra já foi criada!");
+      //  alert("Palavra já foi criada!");
+      removeWord();
+      }
    }
-   
+function removeWord(){
+   let filhos = document.querySelectorAll("input.boxSize:not(input#letraChute.boxSize)");
+   if(filhos.length !== 0){
+      for(let i = 0; i < filhos.length; i++){
+         filhos[i].remove();
+      }
+   }else {
+      alert("Não tem palavra para remover");
+   }
 }
 
 function randomTopic(){
@@ -34,10 +53,70 @@ function randomTopic(){
 function randomWord(topic){
    if(topic === "Pokemon"){
        // console.log(topic + "Right!");
-       let arrayWords = ["pikachu","charmander","dragonite","psyduck"];
-       let randomArrayElement = getRandomInt(0,4);
+       randomArrayElement = getRandomInt(0,4);
        return arrayWords[randomArrayElement];
    }else {
        console.log("Topic is incorrect!");
    }
-}        
+}
+
+// Captura o valor do input do usuário e compara se o valor está na palavra formada
+
+
+function checkWordType(){
+   let flag = 0;
+   let inputDigitado = document.getElementById("letraChute");
+   let valorDigitadoUsuario = inputDigitado.value;
+   for(let i = 0; i < arrayWords[randomArrayElement].length; i++){
+      if(valorDigitadoUsuario === arrayWords[randomArrayElement].charAt(i)){
+         let vetor = document.querySelectorAll("input.boxSize:not(input#letraChute.boxSize)");
+         vetor[i].value = valorDigitadoUsuario;
+         console.log("acertou!");
+         flag++;
+      }
+   }
+   if(flag === 0){
+      tentativas++;
+      alert("Errou!");
+      desenhaBoneco();
+   }
+   
+
+   // console.log("palavra não encontrada!");
+}
+function mostrarDica(){
+
+   let dica = document.getElementById("dica");
+   if(dica.innerHTML === ''){
+      elementoCriado = document.createElement("h2");
+      elementoCriado.textContent = "A dica é Pokêmon!";
+      elementoCriado.classList.add("dicaEstilo");
+      dica.appendChild(elementoCriado);
+   }else {
+      alert("A dica já foi exibida!");
+   }
+   
+}
+function desenhaBoneco(){
+   let referencia;
+   // if(tentativas == 1){
+   //    referencia = document.getElementById("cabeca");
+   //    referencia.classList.add("active");
+   // }else if(tentativas == 2){
+   //       referencia = document.getElementById("tronco");
+   //       referencia.classList.add("active");
+        
+   // }
+   switch (tentativas){
+      case 1: 
+         referencia = document.getElementById("cabeca");
+         referencia.classList.add("active");
+         break;
+      case 2:
+         referencia = document.getElementById("tronco");
+         referencia.classList.add("active");
+         break;
+   }
+        
+   
+}
